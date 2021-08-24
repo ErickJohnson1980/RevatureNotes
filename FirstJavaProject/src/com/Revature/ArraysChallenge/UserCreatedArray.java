41d9a2d77eda36208a87622854f55d7d4ee79838
Erick Johnson <erickjohnson1980>
2021-08-24 13:27:29 -0400

    added solution to array challenge

---
+++ FirstJavaProject/src/com/Revature/ArraysChallenge/UserCreatedArray.java
@@ -0,0 +1,50 @@
+package com.Revature.ArraysChallenge;
+
+import java.util.Scanner;
+
+public class UserCreatedArray {
+
+	static private Scanner scan = new Scanner(System.in);
+	
+	public static int[] CreateArray(int arrayLength)
+	{
+		int[] UserArray = new int[arrayLength];
+		return UserArray;
+	}
+	
+	public static int[] PopulateArray(int[] UserArray)
+	{
+		for(int i=0; i < UserArray.length ; i++)
+		{
+			System.out.println("Please Enter the value of Array["+i+"]. (int)");
+			UserArray[i] = scan.nextInt();
+		}
+		
+		
+		return UserArray;
+	}
+	
+	public static double ArrayAverage(int[] UserArray)
+	{
+		double arrayTotal = 0;
+		for(int i =0 ; i < UserArray.length ; i++)
+		{
+			arrayTotal += UserArray[i];
+		}
+		double avg = arrayTotal / UserArray.length;
+		return avg;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Please Enter the Length of the Array you wish to create.(Int)");
+		int arrayLength = scan.nextInt();
+		
+		int[] UserArray = CreateArray(arrayLength);
+		
+		PopulateArray(UserArray);
+		
+		System.out.println("The average of the values in your array is "+ArrayAverage(UserArray));
+	}
+	
+}
---
+++ FirstJavaProject/src/com/Revature/accessmods/Driver.java
@@ -0,0 +1,5 @@
+package com.Revature.accessmods;
+
+public class Driver {
+
+}
